405bb5505fc1bde1ddba4d42a82af1f29e483f8d
ibo-ibo <zihnimveben@gmail.com>
2022-07-06 21:53:55 +0300

    init dynamic paths

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createRouter } from 'vue-router';
 import { createWebHistory } from 'vue-router';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 import App from './App.vue';
 
 const router = createRouter({
@@ -15,6 +16,10 @@ const router = createRouter({
     {
       path: '/users',
       component: UsersList,
+    },
+    {
+      path: '/teams/:teamId',
+      component: TeamMembers,
     },
   ],
 });

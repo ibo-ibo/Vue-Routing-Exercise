1f8860915cbd4757ddda1370ed7617030f3e5981
ibo-ibo <zihnimveben@gmail.com>
2022-07-06 21:26:55 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue';
+import { createRouter } from 'vue-router';
+import { createWebHistory } from 'vue-router';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+  ],
+});
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
